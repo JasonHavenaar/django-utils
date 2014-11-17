d8f3394a2f85425facd4a0e43f976fecf7da2715
Jason Havenaar <jason@webit.com>
2014-11-17 11:00:23 +1100

    Added login required and auth group required mixins

---
+++ cbv/mixins.py
@@ -0,0 +1,35 @@
+## Mixins to inherit into Class based views
+from django.shortcuts import redirect
+
+class UserCheckMixin(object):
+	# Base Mixin to create permission based checks
+	user_check_failure_path = '/login/'  # can be path, url name or reverse_lazy
+
+	def check_user(self, user):
+		return True
+
+	def user_check_failed(self, request, *args, **kwargs):
+		return redirect(self.user_check_failure_path)
+
+	def dispatch(self, request, *args, **kwargs):
+		if not self.check_user(request.user):
+			return self.user_check_failed(request, *args, **kwargs)
+		return super(UserCheckMixin, self).dispatch(request, *args, **kwargs)
+
+
+
+class LoginRequiredMixin(UserCheckMixin):
+	# Class Based view mixin to replicate the 'login_required' decorator. Redirects on fail
+	def check_user(self, user):
+		return user.is_authenticated()
+
+
+class UserGroupRequiredMixin(UserCheckMixin):
+	# CBV Mixin requiring the current user to be in a givenpermission group.
+	# Set required_group as the 'name' of the permission group
+	required_group = None
+
+	def check_user(self, user):
+		if user.is_authenticated() and self.required_group:
+			return self.required_group in user.groups.all().values_list('name', flat=True)
+		return False
